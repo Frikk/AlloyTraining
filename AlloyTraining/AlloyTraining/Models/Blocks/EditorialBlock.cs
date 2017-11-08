5dfa2cef846cae8eb94adc3b9497a4518759c5c2
fredrik@kurs <no>
2017-11-08 13:23:24 +0100

    Finished D1 -- also fixed missing content areas in ProductPage and its view

---
+++ AlloyTraining/AlloyTraining/Models/Blocks/EditorialBlock.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using EPiServer.Core;
+using EPiServer.DataAbstraction;
+using EPiServer.DataAnnotations;
+
+namespace AlloyTraining.Models.Blocks
+{
+    [ContentType(DisplayName = "Editorial", GUID = "f2740d06-8e6e-4110-805c-5574b138481e",
+        Description = "Use this for rich editorial text that will be reused in multiple places.")]
+    [SiteBlockIcon]
+    public class EditorialBlock : BlockData
+    {
+
+        [CultureSpecific]
+        [Display(Name = "Main body", 
+            Description = "The main body will be shown in the main content area of the pag,e using the XHMTL-editor u can insert for example text images and tables.", 
+            GroupName = SystemTabNames.Content,
+            Order = 10)]
+        public virtual XhtmlString MainBody { get; set; }
+
+    }
+}
--- AlloyTraining/AlloyTraining/Models/Pages/ProductPage.cs
+++ AlloyTraining/AlloyTraining/Models/Pages/ProductPage.cs
@@ -21,6 +21,17 @@ namespace AlloyTraining.Models.Pages
         [UIHint(UIHint.Textarea)]
         [Required]
         public virtual string UniqueSellingPoints  { get; set; }
-         
+
+        [Display(Name = "Main content area",
+            Description = "Drag and drop blocks and pages with partial templates.",
+            GroupName = SystemTabNames.Content,
+            Order = 330)]
+        public virtual ContentArea MainContentArea { get; set; }
+
+        [Display(Name = "Related content area",
+            Description = "Drag and drop blocks and pages with partial templates.",
+            GroupName = SystemTabNames.Content,
+            Order = 340)]
+        public virtual ContentArea RelatedContentArea { get; set; }
     }
 }
